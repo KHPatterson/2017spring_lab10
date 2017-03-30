90a6b16e7e2ac267cf2e589b49c7d6661287ffbc
Dhairya <ddshah42@students.tntech.edu>
2017-03-30 11:29:34 -0500

    notes on how algorithms function

--- Hybrid.h
+++ Hybrid.h
@@ -44,7 +44,16 @@ Hybrid<T>::~Hybrid()
 //simply comment the first implementation out when working on the second implementation
 //use the getKey method to dequeue/remove
 
+// DEQUEUE FUNCTION - first implementation.
+//    FIRST - dequeue to get the Item being removed.  use item->getkey() to get a string*
+//    SECOND - pass sting to finder function.  This returns a doublenode.
+//    THIRD - We call the remove function.  Sorted_list->remove(DBN*)
+//    PROFIT?...
 
+// DEQUEUE FUNCTION - second implementation
+//    FIRST - dequeue the actual queue.  This returns the doublenode (called KILL) with all necessary pointers.
+//    SECOND - set temp pointers to bypass KILL. see pic from pseudo.
+//    THIRD - NULL temp pointers and kill the KILL item with deconstructor
 
 
 
